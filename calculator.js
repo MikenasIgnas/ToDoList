3445f54ddd374e62f6b1ae6709a91cee1ed4fd16
MikenasIgnas <mikenasignas@gmail.com>
2022-06-12 16:53:48 +0300

    Not Finished, does basic calculations

--- calculator.js
+++ calculator.js
@@ -1,14 +1,26 @@
 const screen = document.getElementById("screen");
+const buttons = Array.from(document.getElementsByClassName("button"));
+console.log(buttons);
 
-const buttons = document.getElementsByClassName("button");
+buttons.map((button) => {
+  button.addEventListener("click", (event) => {
+    if (event.target.innerText === "C") {
+      return (screen.innerText = " ");
+    }
 
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", click);
-}
+    if (event.target.innerText === "<-") {
+      return (screen.innerText = screen.innerText.slice(0, -1));
+    }
 
-function click() {
-  const screen = document.getElementById("screen");
-  const buttonclicked = document.getElementById(this.id).value;
+    if (event.target.innerText === "=")
+      return (screen.innerText = eval(screen.innerText));
 
-  screen.innerHTML = buttonclicked;
-}
+    console.log(typeof screen.innerText);
+    screen.innerText += event.target.innerText;
+  });
+});
+//add ERROR if calcultion is invalid
+//add keyboard input
+//css border needs fixint + more style
+//when = is pressed and new number is typed clear screen
+//Math signs that are not on keybord doesnt work with eval(screen.innerText)
