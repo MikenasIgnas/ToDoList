e63814365d70defeaf8a2b0c8074f58c5abaa2b6
MikenasIgnas <mikenasignas@gmail.com>
2022-06-13 23:08:24 +0300

    almost all buttons working, +/- button doesnt work, and some problems with display

--- calculator.js
+++ calculator.js
@@ -1,24 +1,43 @@
 const screen = document.getElementById("screen");
+const memoryScreen = document.getElementById("memoryScreen");
 const buttons = Array.from(document.getElementsByClassName("button"));
 console.log(buttons);
 
 buttons.map((button) => {
   button.addEventListener("click", (event) => {
-    if (event.target.innerText === "C") {
+    if (event.target.innerText === "CE") {
       return (screen.innerText = " ");
+    }
+    if (event.target.innerText === "C") {
+      return (screen.innerText = " "), (memoryScreen.innerText = "");
     }
 
     if (event.target.innerText === "<-") {
       return (screen.innerText = screen.innerText.slice(0, -1));
     }
 
-    if (event.target.innerText === "=")
-      return (screen.innerText = eval(screen.innerText));
+    if (event.target.innerText === "=") {
+      return (screen.innerText = eval(memoryScreen.innerText));
+    }
+    // if (event.target.innerText === "+/-") {
+    //   return (screen.innerText = "-" + screen.innerText);
+    // }
+    if (event.target.innerText === "x^2") {
+      return (screen.innerText = screen.innerText * screen.innerText);
+    }
+    if (event.target.innerText === "√") {
+      return (screen.innerText = Math.sqrt(screen.innerText));
+    }
+    if (event.target.innerText === "1/x") {
+      return (screen.innerText = 1 / screen.innerText);
+    }
 
     console.log(typeof screen.innerText);
-    screen.innerText += event.target.innerText;
+
+    memoryScreen.innerText += event.target.innerText;
   });
 });
+
 //add ERROR if calcultion is invalid
 //add keyboard input
 //css border needs fixint + more style
